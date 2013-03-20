cdae1a15cceea074f5f3b2d4359ed05a51f9208a
dell <dell@1133996>
2013-03-20 23:07:45 +0800

    r

--- src/com/example/mydemo/MainActivity.java
+++ src/com/example/mydemo/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		int a=0;
+		int b=0;
 	}
 
 	@Override
